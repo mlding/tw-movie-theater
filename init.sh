47c61c299161f3e4906cea1b46f02eb563b007fb
Mengli Ding <mlding@thoughtworks.com>
2017-08-03 22:11:54 +0800

    some sql practise

--- init.sh
+++ init.sh
@@ -1,5 +1,39 @@
 #! /bin/bash
 
-cd /Users/mlding/Documents/JS/sqlite-autoconf-3190300
-sqlite3 test.db
+cd /Users/mlding/Documents/JS/tw-movie-theater/theater.db
+sqlite3 theater.db
+
+DROP TABLE IF EXISTS movies;
+CREATE TABLE movies(
+   id int  PRIMARY KEY not null,
+   alt text not null,
+   images text not null,
+   year int not null,
+   directors text not null,
+   title text not null,
+   casts text not null,
+   genres text not null
+);
+
+DROP TABLE IF EXISTS genres;
+CREATE TABLE genres(
+   id int  PRIMARY KEY not null,
+   name text not null
+);
+
+DROP TABLE IF EXISTS movies_enres;
+CREATE TABLE movies_genres(
+   id int  PRIMARY KEY not null,
+   movie_id int not null,
+   genre_id int not null
+);
+
+DROP TABLE IF EXISTS person;
+CREATE TABLE person(
+   id int  PRIMARY KEY not null,
+   name text not null,
+   surname text not null,
+   age int not null
+);
+
 done
